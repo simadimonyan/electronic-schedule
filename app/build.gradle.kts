0a8405fba0cc9b8e183dc0e1dbac2af0ee72525c
Dimitri Simonyan <simadimonyan@gmail.com>
2025-01-26 00:50:02 +0300

    First Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     defaultConfig {
         applicationId = "com.mycollege.schedule"
         minSdk = 27
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
