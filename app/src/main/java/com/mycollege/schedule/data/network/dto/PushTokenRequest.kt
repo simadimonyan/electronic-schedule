026da8bcbc5dd43a972377d8eed0c86dc31f4454
Dimitri Simonyan <simadimonyan@gmail.com>
2025-02-24 21:54:42 +0300

    Release Code 5

--- app/src/main/java/com/mycollege/schedule/data/network/dto/PushTokenRequest.kt
+++ app/src/main/java/com/mycollege/schedule/data/network/dto/PushTokenRequest.kt
@@ -1,7 +1,7 @@
 package com.mycollege.schedule.data.network.dto
 
 data class PushTokenRequest(
-    private val androidId: String,
+    private val os: String,
     private val phoneModel: String,
     private val pushToken: String,
     private val accessToken: String,
